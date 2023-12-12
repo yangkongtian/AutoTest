2667a4f9dce975cfd96dabffb4c621b458355948
unknown <1091860959@qq.com>
2023-12-12 22:58:19 +0800

    提交代码

---
+++ Test.java
@@ -0,0 +1,11 @@
+/**
+ * @author
+ * @param
+ * @return
+ * autoAddLable：https://www.cnblogs.com/gbrr/p/17058982.html
+*/
+public class Test {
+    public int testAdd(int a,int b){
+        return a+b;
+    }
+}
